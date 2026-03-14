8328dddc16469fe6ddb4cf06795b87ef3d73c864
Yosef Jaber <yusefj816@gmail.com>
2026-03-14 19:37:59 -0400

    I think this will work?

--- csim.c
+++ csim.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h>
-#include <math.h>
 
 typedef struct {
     int valid;  
@@ -11,61 +10,63 @@ typedef struct {
 } CacheLine;
 
 typedef struct {
-    CacheLine[] *lines; 
+    CacheLine *lines; 
 } CacheSet;
 
 typedef struct {
-    CacheSet[] *sets;  
+    CacheSet *sets;  
 } Cache;
+
+int s, E, b, byte_offset, set_index, tag, counter, hits, misses, evictions, size; 
+char op;
+unsigned long address;
 
 int least_used_line_index(CacheSet *set) {
     int least_used_line_index = 0;
-    for(int i = 1; i < E, i++) {
-        if(set.lines[i].LRU_val < least_used_line_index) {
+    for(int i = 1; i < E; i++) {
+        if((set->lines[i].LRU_val < set->lines[least_used_line_index].LRU_val) || (set->lines[i].tag == 0 && set->lines[least_used_line_index].LRU_val == 1)) {
             least_used_line_index = i;
         }
     }
     return least_used_line_index;
 }
 
-bool access_memory(int address, Cache *MainMemory) {
-    Result results;
-
-    byte_offset = address & ((b << 1) - 1);
-    set_index = (address >> b) & ((s << 1) - 1);
+void access_memory(int address, Cache *cache) {
+    byte_offset = address & ((1 << b) - 1);
+    set_index = (address >> b) & ((1 << s) - 1);
     tag = (address >> (b+s));
 
-    CacheSet *set = Cache.sets[set_index];
+    CacheSet *set = &cache->sets[set_index];
 
-    for (int i = 0; i < E, i++) {
+    for (int i = 0; i < E; i++) {
         //hit
-        if (set.lines[i].tag == tag) {
-            results.hits++;
-            set.lines[i].LRU_value = counter;
-            return true;
+        if (set->lines[i].tag == tag) {
+            set->lines[i].LRU_val = counter;
+            hits++;
         }
     }
 
     //miss
     int eject_block_index = least_used_line_index(set);
 
+    if(set->lines[eject_block_index].tag == 0) {
+        misses++;
+    }
+    else {
+        misses++;
+        evictions++;
+    }
+
     //Do i have to free memory here before I do this?
     CacheLine new_line;
     new_line.valid = 1;
     new_line.tag = tag;
     new_line.LRU_val = counter;
-    set.lines[eject_block_index] = new_line;
+    set->lines[eject_block_index] = new_line;
 }
 
-int s, E, b, byte_offset, set_index, tag, counter; 
-
-int main(int argc, char *argv[])
-{
-    //s is how many sets there are
-    //E is how many lines in each set there are
-    //b is how many bytes are in each line
+int main(int argc, char *argv[]) {
     int opt;
-
     char *trace_file;
 
     while ((opt = getopt(argc, argv, "s:E:b:t:")) != -1) {
@@ -85,65 +86,45 @@ int main(int argc, char *argv[])
         }
     }
 
-    Cache MainMemory;
+    Cache cache;
+    cache.sets = malloc((1 << s) * sizeof(CacheSet));
 
-    for(int i = 0; i < s; i++)
-    {
-        CacheSet set;
-        for(int j = 0; j < E; j++)
-        {
-            CacheLine line;
-            line.valid = 0x0;
-            line.tag = 0x0;
-            set[j] = line;
+    for (int i = 0; i < (1 << s); i++) {
+        cache.sets[i].lines = malloc(E * sizeof(CacheLine));
+        for (int j = 0; j < E; j++) {
+            cache.sets[i].lines[j].valid = 0;
+            cache.sets[i].lines[j].tag = 0;
         }
-        MainMemory[i] = set;
     }
 
-    FILE *trace_file = fopen(trace_file, "r"); 
+    FILE *file = fopen(trace_file, "r"); 
 
     int hits = 0;
     int misses = 0;
 
-    while (fscanf(trace_file, " %c %lx, %d", &op, &address, &size) = 3) {
-        if(op == "I") {
+    while ((fscanf(file, " %c %lx, %d", &op, &address, &size) == 3)) {
+        if(op == 'I') {
             continue;
         }
 
         // Load or Store
-        if(op == "L" || op == "S") {
+        if(op == 'L' || op == 'S') {
             for (int i = 0; i < size; i++) {
-                if (access_memory(address+i)) {
-                    hits++;
-                }
-                else {
-                    misses++;
-                }
+                access_memory(address+i, &cache);
             }
         }
 
         // Load and Store
-        if(op == "M") {
+        if(op == 'M') {
             for (int i = 0; i < size; i++) {
-                if (access_memory(address+i)) {
-                    hits++;
-                }
-                else {
-                    misses++;
-                }
-
-                if (access_memory(address+i)) {
-                    hits++;
-                }
-                else {
-                    misses++;
-                }
+                access_memory(address+i, &cache);
+                access_memory(address+i, &cache);
             }
         }
         counter++;
     }
 
 
-    printSummary(0, 0, 0);
+    printSummary(hits, misses, evictions);
     return 0;
 }
